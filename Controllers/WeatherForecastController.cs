1baa755568febd0783ea05603a8e12a525e31aa9
Andres Alarcon <andres.alarcon.fonseca@gmail.com>
2023-09-20 21:41:37 -0500

    Implementación de verbos POST y DELETE básicos; Inicialización de lista en memoria y asignación de datos en constructor del controlador

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,20 +13,40 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
+    private static List<WeatherForecast> ListWeatherForeCast = new List<WeatherForecast>();
+
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        if(ListWeatherForeCast == null || !ListWeatherForeCast.Any())
+        {
+            ListWeatherForeCast = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return ListWeatherForeCast;
     }
+    [HttpPost()]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForeCast.Add(weatherForecast);
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        ListWeatherForeCast.RemoveAt(index);
+        return Ok();
+    }
+
 }
